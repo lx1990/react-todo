58b85b7a9268ca7b6f95ce9d32fde02e458ac116
lx1990 <418505763@qq.com>
2019-05-11 15:53:00 +0800

    good

--- src/index.js
+++ src/index.js
@@ -1,9 +1,15 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
+import {render} from 'react-dom';
+import {Provider} from 'react-redux'
+import {createStore} from 'redux'
+import todoApp from './reducers'
+import App from './components/App';
+console.log(todoApp)
+let store = createStore(todoApp);
 
-ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+render(
+    <Provider store={store}>
+        <App/>
+    </Provider>,
+    document.getElementById('root')
 );
